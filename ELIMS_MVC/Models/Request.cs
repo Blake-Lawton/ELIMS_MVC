256614e011f0b0394e1aa493ec190fe597e9ccd7
Remy Brandriff <bsb232@nau.edu>
2019-01-22 08:44:45 -0700

    Commit from Visual Studio

---
+++ ELIMS_MVC/Models/Request.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ELIMS_MVC.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime RequestMade { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NAUEmail { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectObjective { get; set; }
+
+        public string ContactName { get; set; }
+        public string ContactID { get; set; }
+
+        public string Funding { get; set; }
+
+        public string SponsorName { get; set; }
+        public string SponsorPhone { get; set; }
+        public string SponsorEmail { get; set; }
+
+        public string Chemicals { get; set; }
+
+        public string MeetingTimes { get; set; }
+
+    }
+}
