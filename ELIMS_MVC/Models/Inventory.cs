fbf1ed951c0d1a89b5d5fdc1742ee57c3743b930
jwb265 <julian.bell2@gmail.com>
2019-02-19 10:56:00 -0700

    Updated Inventory.cs to reflect chemical inventory

--- ELIMS_MVC/Models/Inventory.cs
+++ ELIMS_MVC/Models/Inventory.cs
@@ -7,19 +7,40 @@ namespace ELIMS_MVC.Models
 {
     public class Inventory
     {
+        // Preliminary information of chemical for inventory
         public int Id { get; set; }
         public string Name { get; set; }
-
         public string CASNum { get; set; }
-        public string EINECSNum { get; set; }
 
-        public string Classification { get; set; }
-        public string HazardPhrases { get; set; }
+        // Who made the chemical / where was it obtained?
+        public string Manufacturer { get; set; }
 
-        public string Tonnage { get; set; }
+        // Does the chemical have a data sheet that can be attached to it?
+        public bool HasMSDS { get; set; }
+
+        // Safety diamond information
+        public int HealthHazard { get; set; }
+        public int FlammableHazard { get; set; }
+        public int ReactiveHazard { get; set; }
+        public int OtherHazard { get; set; }
+
+        // How much is there right now?
+        public int CurrentQty { get; set; }
+        public string Unit { get; set; }
 
         // Include place for type, so we could potentially also list materials, appliances, etc. as well as substances and mixtures
-        public string EntryType { get; set; }
-        
+        public Date DateReceived { get; set; }
+        public bool IsCheckedOut { get; set; }
+        public Date DateCheckedOut { get; set; }
+        public Date DateReturned { get; set; }
+
+        // Where is the chemical currently being stored?
+        public string Location { get; set; }
+
+        // Disposal stats
+        public bool Consumed { get; set; }
+        public bool HazWaste { get; set; }
+        public bool Disposed { get; set; }
+
     }
 }
