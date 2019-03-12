1af672427d7390b0c547c96213ea357836070f6d
Remy Brandriff <36282991+remyBrandriff@users.noreply.github.com>
2019-03-11 22:32:46 -0700

    Add files via upload

---
+++ ELIMS_MVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ELIMS_MVC.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ELIMS_MVC.Areas.Identity.IdentityHostingStartup))]
+namespace ELIMS_MVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
